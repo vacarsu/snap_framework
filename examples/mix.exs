1f8aa9593ba73172e0cce2ea1f353a2f640427a2
Vacarsu <kasvaca@gmail.com>
2021-10-21 10:03:20 -0700

    use hex version of scenic 0.11

--- examples/mix.exs
+++ examples/mix.exs
@@ -22,10 +22,10 @@ defmodule Examples.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:scenic, git: "https://github.com/boydm/scenic.git", branch: "v0.11"},
-      {:scenic_driver_glfw, git: "https://github.com/boydm/scenic_driver_glfw.git", branch: "v0.11"},
+      {:scenic, "~> 0.11.0-beta.0"},
+      {:scenic_driver_local, "~> 0.11.0-beta.0"},
 
-      {:truetype_metrics, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:truetype_metrics, "~> 0.5"},
       {:ex_image_info, "~> 0.2.4", runtime: false},
 
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
--- mix.exs
+++ mix.exs
@@ -23,11 +23,9 @@ defmodule SnapFramework.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:scenic, git: "https://github.com/boydm/scenic.git", branch: "v0.11"},
-      {:scenic_driver_glfw, git: "https://github.com/boydm/scenic_driver_glfw.git", branch: "v0.11"},
-
-      {:truetype_metrics, "~> 0.5", only: [:dev, :test], runtime: false},
-
+      {:scenic, "~> 0.11.0-beta.0"},
+      {:scenic_driver_local, "~> 0.11.0-beta.0"},
+      {:truetype_metrics, "~> 0.5"},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
       {:ring_logger, "~> 0.6"},
       {:map_diff, "~> 1.3.4"}
