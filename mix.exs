f677a3bccf2f31c30ce136948431c6dec467cc6f
Vacarsu <kasvaca@gmail.com>
2021-12-02 08:47:25 -0800

    bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule SnapFramework.MixProject do
   use Mix.Project
 
-  @version "0.1.0-alpha.5"
+  @version "0.1.0-alpha.6"
   @github "https://github.com/vacarsu/snap_framework"
 
   def project do
